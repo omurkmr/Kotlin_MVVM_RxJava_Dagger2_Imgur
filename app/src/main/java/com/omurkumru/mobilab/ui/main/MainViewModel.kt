ccb44da187013c5a61ec852cf8422f25cf7419ee
Ömür Kumru <omurkumru@outlook.com>
2018-12-14 05:55:15 +0100

    fixed one time disposableObserver subscribe issue

--- app/src/main/java/com/omurkumru/mobilab/ui/main/MainViewModel.kt
+++ app/src/main/java/com/omurkumru/mobilab/ui/main/MainViewModel.kt
@@ -18,27 +18,7 @@ class MainViewModel @Inject constructor(
 
     var imageResult: MutableLiveData<List<MainImage>> = MutableLiveData()
 
-    var disposableObserver = object : DisposableObserver<RawGalleryResponse>() {
-        var imageList: ArrayList<MainImage> = ArrayList()
-        override fun onComplete() {}
-        override fun onNext(images: RawGalleryResponse) {
-            val list = images.data
-            //we cannot get one type data list so we have create our own list
-            //onNext method fits perfectly on this situation
-            list?.forEach {
-                if (it.isAlbum!!) {
-                    it.images?.forEach {
-                        imageList.add(MainImage(it.id, it.link, it.description))
-                    }
-                } else {
-                    imageList.add(MainImage(it.id, it.link, it.description))
-                }
-            }
-            imageResult.postValue(imageList)
-        }
-
-        override fun onError(e: Throwable) {}
-    }
+    lateinit var disposableObserver: DisposableObserver<RawGalleryResponse>
 
     var cacheOption = arrayOf(CacheTypeConstants.IN_MEMORY, CacheTypeConstants.ON_DISK)
     var sectionOption = arrayOf(SectionConstants.HOT, SectionConstants.TOP, SectionConstants.USER)
@@ -46,6 +26,28 @@ class MainViewModel @Inject constructor(
     var windowOption = arrayOf(WindowTypeConstants.DAY, WindowTypeConstants.WEEK, WindowTypeConstants.MONTH, WindowTypeConstants.ALL)
 
     fun getGalleryImages(section: String, sort: String, window: String, showViral: Boolean) {
+
+        disposableObserver = object : DisposableObserver<RawGalleryResponse>() {
+            var imageList: ArrayList<MainImage> = ArrayList()
+            override fun onComplete() {}
+            override fun onNext(images: RawGalleryResponse) {
+                val list = images.data
+                //we cannot get one type data list so we have create our own list
+                //onNext method fits perfectly on this situation
+                list?.forEach {
+                    if (it.isAlbum!!) {
+                        it.images?.forEach {
+                            imageList.add(MainImage(it.id, it.link, it.description))
+                        }
+                    } else {
+                        imageList.add(MainImage(it.id, it.link, it.description))
+                    }
+                }
+                imageResult.postValue(imageList)
+            }
+
+            override fun onError(e: Throwable) {}
+        }
 
         mainRepository.getGalleryImagesFromApi(section, sort, window, showViral)
                 //making rest requests so using io fits
